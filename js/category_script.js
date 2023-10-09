448af7951f24e9a364503dcddc663c8daf75e36a
unknown <eldik.grom@google.com>
2023-10-09 20:12:29 +0300

    second commit

---
+++ js/category_script.js
@@ -0,0 +1,14 @@
+const main = document.querySelector(".main")
+
+main.innerHTML = `
+<div class="home">
+        <a href="image_api.html">Home</a>
+        <a href="cart.html">
+            <i class="fa fa-shopping-cart" style="font-size:24px"></i>
+        </a>
+</div>
+<div class="categories"></div>
+       
+`
+
+
